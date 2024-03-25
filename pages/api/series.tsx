517d3ea3a2f51f26d49d131caedebcb243afddc2
Dg <dnechtan@gmail.com>
2024-03-25 00:38:45 -0300

    [feature] realtime data with api

--- pages/api/series.tsx
+++ pages/api/series.tsx
@@ -1,37 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { Series } from "../../interfaces";
+import type { Series } from "@/interfaces";
 
-
+function random(len:number = 23, min:number = 0, max:number = 100) {
+    const ret = [];
+    ++len; --max;
+    for (let i = min; i < len; i++) {
+        ret.push(Math.floor(Math.random() * max));
+    }
+    return ret
+}
 
 export default function handler(
-  _req: NextApiRequest,
-  res: NextApiResponse<Series[]>
+    _req: NextApiRequest,
+    res: NextApiResponse<Series[]>
 ) {
 
-function random(){
-    var numerosAleatorios = [];
-for (var i = 0; i < 24; i++) {
-    numerosAleatorios.push(Math.floor(Math.random() * 101));
-} 
-return numerosAleatorios
-  
-}   
+    const series: Series[] = [{
+        name: 'Sensor 1',
+        data: random(),
+    }, {
+        name: 'Sensor 2',
+        data: random(),
+    }, {
+        name: 'Sensor 3',
+        data: random(),
+    }
+    ]
 
-
-const series: Series[] = [{
-    name:'Sensor 1',
-    data: random(),
-},{
-    name:'Sensor 2',
-    data: random(),
-},{
-    name:'Sensor 3',
-    data: random(),
-}
-]
-
-
-
-
-  res.status(200).json(series);
+    res.status(200).json(series);
 }
--- pages/todos/index.tsx
+++ pages/todos/index.tsx
@@ -2,27 +2,24 @@ import styles from '@/styles/Todos.module.css'
 
 export async function getStaticProps() {
     const data = await fetch('http://localhost:3000/api/todos')
-
     const todos = await data.json()
-
-   
 
     return {
         props: { todos },
     }
 }
 
-export default function Todos({ todos }){
-    return(
+export default function Todos({ todos }: any) {
+    return (
         <>
-        
+
             <h1>Tarefas para fazer</h1>
             <ul className={styles.todolist}>
-                {todos.map((todo) => (
+                {todos.map((todo: any) => (
                     <li key={todo.id}>{todo.title}</li>
                 ))}
             </ul>
-   
+
         </>
     )
 }
