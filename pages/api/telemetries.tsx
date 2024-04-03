fe88e85924bfa757bd9c831f7a656ad82ee2ab83
Dg <dnechtan@gmail.com>
2024-04-03 10:47:39 -0300

    bugfix: Selects do not reset and persist when selecting 'machine'
    The select inputs were not being reset when the 'machine' option was selected, causing previously selected values to persist. This has been fixed.
    
    feat: Allow multiple machines to be selected
    
    Added functionality to allow selecting multiple machines from the select input.

--- pages/api/telemetries.tsx
+++ pages/api/telemetries.tsx
@@ -1,18 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import  fs  from 'fs';
-
-
-
+import fs from 'fs';
 
 export default function handler(
   _req: NextApiRequest,
   res: NextApiResponse<[]>,
 ) {
 
+  const machineIds: string[] = _req?.query?.machines.split(',') || [];
 
+  let machines: string[] = [];
 
-const file = fs.readFileSync(  __dirname + '/../../../../data/telemetries/' + _req.query.machine + '.json', 'utf8');
-  const data = JSON.parse(file);
+  console.log('is machineIds', machineIds);
+  if (machineIds.length) {
+    machineIds.map((id: any) => {
+      if (!id) return;
+      let file = fs.readFileSync(__dirname + '/../../../../data/telemetries/' + id + '.json', 'utf8');
+      let data = JSON.parse(file);
+      machines = machines.concat(data)
+    })
+  }
 
-  res.status(200).json(data);
+  console.log('machines', machines);
+
+  // data.filter((machine) => machine.idvalue ==  )
+  res.status(200).json(machines);
 }
