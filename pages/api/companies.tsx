efaae466a7c320b6aec69caf2bb4ee5fe3985a33
Dg <dnechtan@gmail.com>
2024-04-15 10:28:02 -0300

    nextjs14.2 v02

--- pages/api/companies.tsx
+++ pages/api/companies.tsx
@@ -2,6 +2,8 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import  fs  from 'fs';
 import Companies from '../../data/companies/companies.json';
 
+console.log(process.env);
+
 export default function handler(
   _req: NextApiRequest,
   res: NextApiResponse<any[]>,
