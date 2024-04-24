76ef57990e7e6dd9c80eb19192a21aa703284855
Felipe Stamboni <123281130+felpzsccp@users.noreply.github.com>
2024-04-24 11:41:16 -0300

    Arrumei o erro que estava dando no console na api de telemetries

--- pages/api/telemetries.tsx
+++ pages/api/telemetries.tsx
@@ -7,12 +7,14 @@ export default function handler(_req: NextApiRequest, res: NextApiResponse<[]>) 
   let machines: string[] = [];
 
   if (machineIds.length) {
-    machineIds.map((id: any) => {
-      if (!id) return;
-      let file = fs.readFileSync(__dirname + "/../../../../data/telemetries/" + id + ".json", "utf8");
-      let data = JSON.parse(file);
-      machines = machines.concat(data);
-    });
+    try {
+      machineIds.map((id: any) => {
+        if (!id) return;
+        let file = fs.readFileSync(__dirname + "/../../../../data/telemetries/" + id + ".json", "utf8");
+        let data = JSON.parse(file);
+        machines = machines.concat(data);
+      });
+    } catch (error) {}
   }
 
   res.status(200).json(machines);
