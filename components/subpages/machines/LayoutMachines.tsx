0c222545c9e7a600144bdca7c93aacd01dd20599
felipestambonidev <felipestambonii@gmail.com>
2024-07-25 17:31:48 -0300

    edit. I changed the re-registration header to the Machines filter

--- components/subpages/machines/LayoutMachines.tsx
+++ components/subpages/machines/LayoutMachines.tsx
@@ -1,13 +1,14 @@
 import HeaderSubPage from "@/components/headersubpage/HeaderSubPage";
 import TitleMachines from "./TitleMachines";
 import TableMachines from "./TableMachines";
+import Welcome from "@/components/overview/Welcome";
 
 
 export default function LayoutMachines(){
     return(
         <section>
             <div>
-                <HeaderSubPage/>
+                <Welcome/>
             </div>
             <div className="bg-white">
                 <TitleMachines />
